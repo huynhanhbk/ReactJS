5ec47f0c995a1b7a73e25deb399e1687ef760d9f
ANHHUYNH-PC <huynhanhbk.92@gmail.com>
2022-06-17 00:21:17 +0700

    first commit lab05

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React, { Component } from 'react';
-import './App.css';
-class App extends Component {
+import "./App.css";
+import React, { Component } from "react";
+import Main from "./components/MainComponent";
 
+class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>Sample cho toàn bộ lab và project trong môn học Lập Trình Web Front-End với React với đầy đủ dependency.</p>
-        <p>Phiên bản React hiện tại trên máy của bạn là: {React.version}</p>
-        <p>Phiên bản React đã được sử dụng để thiết kế môn học: 16.14.0.</p>
+        <Main />
       </div>
     );
   }
 }
 
 export default App;
+
+//----Lab 4.1
+
+/*import "./App.css";
+import React, { Component } from "react";
+import Main from "./components/MainComponent";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Main />
+      </div>
+    );
+  }
+}
+
+export default App;
+*/
