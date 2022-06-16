c30f8a6581b118d1753674b6b3e6af95da6c1550
ANHHUYNH-PC <huynhanhbk.92@gmail.com>
2022-06-16 15:02:42 +0700

    update lab04

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React, { Component } from 'react';
-import './App.css';
-class App extends Component {
+import "./App.css";
+import React, { Component } from "react";
+import Menu from "./components/MenuComponent";
+import { Navbar, NavbarBrand } from "reactstrap";
+import { DISHES } from "./shared/dishes";
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+  }
   render() {
     return (
       <div className="App">
-        <p>Sample cho toàn bộ lab và project trong môn học Lập Trình Web Front-End với React với đầy đủ dependency.</p>
-        <p>Phiên bản React hiện tại trên máy của bạn là: {React.version}</p>
-        <p>Phiên bản React đã được sử dụng để thiết kế môn học: 16.14.0.</p>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">
+              Ristorante Con Fusion - Exercise 1
+            </NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
