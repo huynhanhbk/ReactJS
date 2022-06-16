7a6b80da573ce94004cc7d3632300b68780a0b8e
ANHHUYNH-PC <huynhanhbk.92@gmail.com>
2022-06-16 15:36:07 +0700

    cap nhat lai tat ca cac component

---
+++ src/components/MainComponent.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import Menu from "./MenuComponent";
+import { Navbar, NavbarBrand } from "reactstrap";
+import { DISHES } from "../shared/dishes";
+import DishDetail from "./DishDetailComponent";
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+      selectedDish: null,
+    };
+  }
+  onDishSelect(dishId) {
+    this.setState({ selectedDish: dishId });
+  }
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">
+              Ristorante Con Fusion - Exercise 1
+            </NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu
+          dishes={this.state.dishes}
+          onClick={(dishId) => this.onDishSelect(dishId)}
+        />
+        <DishDetail
+          dish={
+            this.state.dishes.filter(
+              (dish) => dish.id === this.state.selectedDish
+            )[0]
+          }
+        />
+      </div>
+    );
+  }
+}
+
+export default Main;
