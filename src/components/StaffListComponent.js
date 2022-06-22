8c80127155f09eca82daefd3f305cb14dc5de9dd
ANHHUYNH-PC <huynhanhbk.92@gmail.com>
2022-06-23 00:35:52 +0700

    hoan thanh tinh nang search nhan vien

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
 import { Card, CardImg } from "reactstrap";
 import { Link } from "react-router-dom";
+// import TextField from "@material-ui/core/TextField";
 
 function RenderMenuStaffs({ staff, onClick }) {
   return (
@@ -13,23 +14,68 @@ function RenderMenuStaffs({ staff, onClick }) {
   );
 }
 
-function StaffList(props) {
-  const danhsachnv = props.staffs.map((staff) => {
+class StaffList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterArray: [],
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange = (event) => {
+    let currentList = [];
+    let newList = [];
+    if (event.target.value !== "") {
+      currentList = this.props.staffs;
+      newList = currentList.filter((item) => {
+        return item.name
+          .toLowerCase()
+          .includes(event.target.value.toLowerCase());
+      });
+    } else {
+      newList = this.props.staffs;
+    }
+    this.setState({
+      filterArray: newList,
+    });
+  };
+
+  render() {
+    const danhsachnv = this.state.filterArray.map((staff) => {
+      return (
+        <div key={staff.id} className="col-6 col-sm-4 col-md-2 mt-4">
+          <RenderMenuStaffs staff={staff} />
+        </div>
+      );
+    });
     return (
-      <div key={staff.id} className="col-6 col-sm-4 col-md-2 mt-4">
-        <RenderMenuStaffs staff={staff} />
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-3">
+            <h4>Danh sách Nhân Viên</h4>
+          </div>
+          <div className="text-right col-12 col-md-9">
+            <input
+              type="text"
+              className="input mt-2"
+              onChange={this.handleInputChange}
+              placeholder="Tim kiem"
+            />
+          </div>
+        </div>
+        <hr />
+        <div className="row">{danhsachnv}</div>
+        <br></br>
       </div>
     );
-  });
-  return (
-    <div className="container">
-      <div className="row">
-        <h4>Danh sách Nhân Viên</h4>
-      </div>
-      <hr />
-      <div className="row">{danhsachnv}</div>
-      <br></br>
-    </div>
-  );
+  }
+
+  componentDidMount() {
+    this.setState({
+      filterArray: this.props.staffs,
+    });
+  }
 }
 export default StaffList;
