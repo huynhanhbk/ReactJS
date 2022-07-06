27e45bb8443c85265fbf92fc45eab7a7ae16c267
ANHHUYNH-PC <huynhanhbk.92@gmail.com>
2022-07-07 02:29:22 +0700

    khoi tao ex4

--- src/redux/comments.js
+++ src/redux/comments.js
@@ -22,8 +22,8 @@ export const Comments = (
       };
     case ActionTypes.ADD_COMMENT:
       var comment = action.payload;
-      comment.id = state.comments.length;
-      comment.date = new Date().toISOString();
+      //comment.id = state.comments.length; remove khi them postComment vi id may chu tu cap nhat
+      //comment.date = new Date().toISOString();
       console.log("Comment: ", comment);
       return { ...state, comments: state.comments.concat(comment) };
 
