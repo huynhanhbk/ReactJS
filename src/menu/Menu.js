e31fd75d925aba646b66ea9254db6ae1560a74a6
ANHHUYNH-PC <huynhanhbk.92@gmail.com>
2022-10-05 12:40:12 +0700

    Add css transition on Food hover

--- src/menu/Menu.js
+++ src/menu/Menu.js
@@ -12,9 +12,11 @@ const MenuStyled = styled.div`
 export function Menu() {
   return (
     <MenuStyled>
+      {/* Trả về mảng thuộc tính đếm được của các cặp [key, value] với object đã cho, tương tự như dùng vặp lặp for...in. 
+      Object.entries() là một method tạo một nested array với key/value của một Object.*/}
       {Object.entries(foods).map(([sectionName, foods]) => (
         <>
-          <h1>Menu</h1>
+          <h1>{sectionName}</h1>
           <FoodGrid>
             {foods.map((food) => (
               <Food img={food.img}>
